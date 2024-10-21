3acca1c8bd7c96bc21f868f7a5df69995507b087
root <root@chestnuts166.localdomain>
2024-10-21 20:53:50 +0900

    authoriztion setup

---
+++ app/javascript/env.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCu_STes4UffeW4OTij5l_ISVKbXAr9pJQ",
+  authDomain: "fir-test-2d926.firebaseapp.com",
+  projectId: "fir-test-2d926",
+  storageBucket: "fir-test-2d926.appspot.com",
+  messagingSenderId: "795944425886",
+  appId: "1:795944425886:web:af926a572a70cb93381e8f"
+}
+
+export { firebaseConfig }
