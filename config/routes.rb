369fd6139f5dc7a254533aaef3c5b4b50dc8fd9a
root <root@chestnuts166.localdomain>
2024-10-23 16:05:30 +0900

    create signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "users/create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,4 +13,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "top#index"
   get "mypage", to: "mypage#index"
+
+  resources :users, only: [ :index, :new, :create ]
 end
